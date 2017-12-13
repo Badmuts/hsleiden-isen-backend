d522e46f28e7ab39b5fd293afe0f64c5dfc13d7f
Daan Rosbergen <d.rosbergen@gmail.com>
2017-12-13 13:15:10 +0100

    Use .env and add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# HSLEIDEN ISEN Backend
+
+## Getting started
+
+```sh
+$ cp .env.example .env
+$ yarn
+$ yarn start
+```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Universe!'))
 
-app.listen(1337, () => console.log('Example app listening on port 1337!'))
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))

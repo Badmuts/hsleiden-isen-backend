220744ddb234e458db596423159b9d5fd5af8d1e
Daan Rosbergen <d.rosbergen@gmail.com>
2017-12-13 21:26:08 +0100

    Add morgan request logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const Sequelize = require('sequelize')
+const morgan = require('morgan')
+
 const app = express()
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
   dialect: 'mysql'
 });
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/healthz', (req, res) => {

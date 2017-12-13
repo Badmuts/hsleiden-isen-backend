bb4f2ca19524d103b4c5280bc82b74f3f4d2ac6a
Daan Rosbergen <d.rosbergen@gmail.com>
2017-12-13 18:00:57 +0100

    Add sql client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
+const express = require('express')
+const Sequelize = require('sequelize')
+const app = express()
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: 'db',
+  dialect: 'mysql'
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/healthz', (req, res) => {
+	sequelize.authenticate()
+	    .then(() => res.json({healthy: true, error: null}))
+	    .catch(err => res.json({healthy: false, error: err}))
+})
 
 app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))

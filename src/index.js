aecf6a9f44cfda31d4d3b12b9415d2204c28c9fe
Daan Rosbergen <d.rosbergen@gmail.com>
2018-01-25 15:09:04 +0100

    Add version to healthz endpoint

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(
 );
 
 const { connect } = require("./ttn/client");
+const VERSION = require('./../package.json').version;
 
 app.use(morgan("combined"));
 
@@ -22,13 +23,13 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/healthz", (req, res) => {
   sequelize
     .authenticate()
-    .then(() => res.json({ healthy: true, error: null }))
-    .catch(err => res.json({ healthy: false, error: err }));
+    .then(() => res.json({ healthy: true, error: null, VERSION }))
+    .catch(err => res.json({ healthy: false, error: err, VERSION }));
 });
 
 // Connect to TTN
 connect();
 
 app.listen(process.env.PORT, () =>
-  console.log(`App listening on port ${process.env.PORT}!`)
+  console.log(`App ${VERSION} listening on port ${process.env.PORT}!`)
 );
